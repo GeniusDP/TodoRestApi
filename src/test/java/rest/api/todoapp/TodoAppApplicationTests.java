da799157491c71a4eb29827d44414643eedee2c2
GeniusDP <zaranikbz@gmail.com>
2022-06-14 15:47:39 +0300

    initial

---
+++ src/test/java/rest/api/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.api.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
