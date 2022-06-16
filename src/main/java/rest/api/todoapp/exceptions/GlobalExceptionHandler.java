ee8256a9f0c4d4b764570e490426e64233163834
GeniusDP <zaranikbz@gmail.com>
2022-06-16 13:32:21 +0300

    without exception handling

--- src/main/java/rest/api/todoapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/rest/api/todoapp/exceptions/GlobalExceptionHandler.java
@@ -13,43 +13,43 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoElementsException.class)
-    public ResponseEntity<ExceptionResponseEntity> noElementsExceptionHandler(NoElementsException exception){
-        String comment = exception.getLocalizedMessage();
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .body(new ExceptionResponseEntity(comment, LocalDateTime.now()));
-    }
-
-    @ExceptionHandler(NoSuchTodoException.class)
-    public ResponseEntity<ExceptionResponseEntity> noSuchTodoExceptionHandler(NoSuchTodoException e){
-        String comment = e.getMessage();
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
-    }
-
-
-
-
-
-    @ExceptionHandler({DataAccessException.class, SQLException.class})
-    public ResponseEntity<ExceptionResponseEntity> dataAccessExceptionHandler(DataAccessException exception){
-        String comment = "Error during data processing or accessing.";
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ExceptionResponseEntity> illegalArgumentAndNullPointerExceptionHandler(){
-        String comment = "You cannot pass such values of parameters!";
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
-    }
-
-    @ExceptionHandler(NotAllParametersInUrlException.class)
-    public ResponseEntity<ExceptionResponseEntity> notAllParametersInUrlExceptionHandler(NotAllParametersInUrlException e){
-        String comment = e.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
-    }
+//    @ExceptionHandler(NoElementsException.class)
+//    public ResponseEntity<ExceptionResponseEntity> noElementsExceptionHandler(NoElementsException exception){
+//        String comment = exception.getLocalizedMessage();
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+//                .body(new ExceptionResponseEntity(comment, LocalDateTime.now()));
+//    }
+//
+//    @ExceptionHandler(NoSuchTodoException.class)
+//    public ResponseEntity<ExceptionResponseEntity> noSuchTodoExceptionHandler(NoSuchTodoException e){
+//        String comment = e.getMessage();
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+//                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
+//    }
+//
+//
+//
+//
+//
+//    @ExceptionHandler({DataAccessException.class, SQLException.class})
+//    public ResponseEntity<ExceptionResponseEntity> dataAccessExceptionHandler(DataAccessException exception){
+//        String comment = "Error during data processing or accessing.";
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
+//    }
+//
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ResponseEntity<ExceptionResponseEntity> illegalArgumentAndNullPointerExceptionHandler(){
+//        String comment = "You cannot pass such values of parameters!";
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
+//    }
+//
+//    @ExceptionHandler(NotAllParametersInUrlException.class)
+//    public ResponseEntity<ExceptionResponseEntity> notAllParametersInUrlExceptionHandler(NotAllParametersInUrlException e){
+//        String comment = e.getMessage();
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
+//    }
 
 }
