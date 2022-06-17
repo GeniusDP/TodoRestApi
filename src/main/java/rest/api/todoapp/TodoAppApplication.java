67b1e47b96c37430f616644f3d6b3438a4449b5a
GeniusDP <zaranikbz@gmail.com>
2022-06-17 21:18:02 +0300

    moved SpringBootApplication class into the main package

--- src/main/java/rest/api/todoapp/TodoAppApplication.java
+++ src/main/java/rest/api/todoapp/TodoAppApplication.java
@@ -1,15 +1,11 @@
-package rest.api.todoapp.configuration;
+package rest.api.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("rest.api.todoapp")
 public class TodoAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
-
 }
