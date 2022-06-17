54bfb4c0f7daf4af110a83f15b38d3e8ae854ce6
GeniusDP <zaranikbz@gmail.com>
2022-06-17 21:29:26 +0300

    controller main path and autowired there

--- src/main/java/rest/api/todoapp/controllers/TodoController.java
+++ src/main/java/rest/api/todoapp/controllers/TodoController.java
@@ -1,10 +1,8 @@
 package rest.api.todoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import rest.api.todoapp.exceptions.NoElementsException;
 import rest.api.todoapp.services.dto.request.PaginationRequestTodoDTO;
 import rest.api.todoapp.services.dto.request.TodoRequestDTO;
 import rest.api.todoapp.services.TodoService;
@@ -15,38 +13,43 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
-    @Autowired
-    private TodoService service;
+    private final TodoService service;
 
-    @GetMapping("/todos")
+    @Autowired
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos() {
         return ResponseEntity.ok( service.getAllTodos() );
     }
 
-    @GetMapping("/todos/{todoId}")
+    @GetMapping("/{todoId}")
     public ResponseEntity<Todo> getTodoById(@PathVariable UUID todoId){
         return ResponseEntity.ok( service.getTodoById(todoId) );
     }
 
-    @GetMapping("/todos/paginated")
+    @GetMapping("/paginated")
     public ResponseEntity<List<Todo>> getPaginatedTodoList(PaginationRequestTodoDTO paginationDTO){
         return ResponseEntity.ok( service.getPaginatedTodoList(paginationDTO) );
     }
 
-    @PostMapping("/todos")
+    @PostMapping
     public ResponseEntity<UUID> saveTodoItem(@RequestBody TodoRequestDTO todoRequestDTO){
         UUID todoId = service.saveTodoItem(todoRequestDTO.getTitle(), todoRequestDTO.getBody());
         return ResponseEntity.ok(todoId);
     }
 
-    @PutMapping("/todos/{todoId}")
+    @PutMapping("/{todoId}")
     public ResponseEntity<String> updateTodoById(@PathVariable UUID todoId, @RequestParam Optional<String> title, @RequestParam Optional<String> body){
         String message = String.format("todo with id = %s successfully updates", service.updateTodoById(todoId, title, body));
         return ResponseEntity.ok(message);
     }
 
-    @DeleteMapping("/todos/{todoId}")
+    @DeleteMapping("/{todoId}")
     public ResponseEntity<String> deleteTodoById(@PathVariable UUID todoId){
         String message = String.format( "todo with id = %s successfully deleted", service.deleteTodoById(todoId) );
         return ResponseEntity.ok( message );
