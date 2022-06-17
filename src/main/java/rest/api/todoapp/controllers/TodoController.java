90cb9669722d22eb6246ab312a974ed680f62b12
GeniusDP <zaranikbz@gmail.com>
2022-06-17 21:34:42 +0300

    response status for DeleteMapping

--- src/main/java/rest/api/todoapp/controllers/TodoController.java
+++ src/main/java/rest/api/todoapp/controllers/TodoController.java
@@ -1,6 +1,7 @@
 package rest.api.todoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest.api.todoapp.services.dto.request.PaginationRequestTodoDTO;
@@ -48,6 +49,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{todoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public String deleteTodoById(@PathVariable UUID todoId){
         return String.format( "todo with id = %s successfully deleted", service.deleteTodoById(todoId) );
     }
