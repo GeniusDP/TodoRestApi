122c88e374a49421f10137eea84dd5802880ee5b
GeniusDP <zaranikbz@gmail.com>
2022-06-18 09:45:36 +0300

    Revert "made GlobalExceptionHandler much easier to read"
    This reverts commit 84199516d8e2599de41c54169ec002aad687e416.

--- src/main/java/rest/api/todoapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/rest/api/todoapp/exceptions/GlobalExceptionHandler.java
@@ -2,9 +2,9 @@ package rest.api.todoapp.exceptions;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import rest.api.todoapp.services.dto.response.ExceptionResponseEntity;
 
 import java.sql.SQLException;
@@ -14,34 +14,42 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoElementsException.class)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ExceptionResponseEntity noElementsExceptionHandler(NoElementsException exception){
-        return new ExceptionResponseEntity(exception.getMessage(), LocalDateTime.now());
+    public ResponseEntity<ExceptionResponseEntity> noElementsExceptionHandler(NoElementsException exception){
+        String comment = exception.getLocalizedMessage();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(new ExceptionResponseEntity(comment, LocalDateTime.now()));
     }
 
     @ExceptionHandler(NoSuchTodoException.class)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ExceptionResponseEntity noSuchTodoExceptionHandler(NoSuchTodoException e){
-        return new ExceptionResponseEntity(e.getMessage(), LocalDateTime.now());
+    public ResponseEntity<ExceptionResponseEntity> noSuchTodoExceptionHandler(NoSuchTodoException e){
+        String comment = e.getMessage();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
     }
+
+
+
 
 
     @ExceptionHandler({DataAccessException.class, SQLException.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ExceptionResponseEntity dataAccessExceptionHandler(){
-        return new ExceptionResponseEntity("Error during data processing or accessing.", LocalDateTime.now());
+    public ResponseEntity<ExceptionResponseEntity> dataAccessExceptionHandler(DataAccessException exception){
+        String comment = "Error during data processing or accessing.";
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ExceptionResponseEntity illegalArgumentAndNullPointerExceptionHandler(){
-        return new ExceptionResponseEntity("You cannot pass such values of parameters!", LocalDateTime.now());
+    public ResponseEntity<ExceptionResponseEntity> illegalArgumentAndNullPointerExceptionHandler(){
+        String comment = "You cannot pass such values of parameters!";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
     }
 
     @ExceptionHandler(NotAllParametersInUrlException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ExceptionResponseEntity notAllParametersInUrlExceptionHandler(NotAllParametersInUrlException e){
-        return new ExceptionResponseEntity(e.getMessage(), LocalDateTime.now());
+    public ResponseEntity<ExceptionResponseEntity> notAllParametersInUrlExceptionHandler(NotAllParametersInUrlException e){
+        String comment = e.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body( new ExceptionResponseEntity(comment, LocalDateTime.now()) );
     }
 
 }
