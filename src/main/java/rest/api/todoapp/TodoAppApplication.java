a8948502e58d8f3332648a03883a2e4561adf5a6
GeniusDP <zaranikbz@gmail.com>
2022-06-23 23:38:14 +0300

    TodoAppApplication class: time zone setting

--- src/main/java/rest/api/todoapp/TodoAppApplication.java
+++ src/main/java/rest/api/todoapp/TodoAppApplication.java
@@ -3,9 +3,16 @@ package rest.api.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TodoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
+	}
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+3"));
 	}
 }
